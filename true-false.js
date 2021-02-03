f5a8a82693212e56f24de3b92b481aefce830c61
mdshahedarman <m.s.arman4244@gmail.com>
2021-02-04 01:18:07 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+//falsy
+// 0
+//undefined
+//null
+//NaN
+//false
+//truly
+//'0'
+//"0"
+//[]
+let age = "";
+if (age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
